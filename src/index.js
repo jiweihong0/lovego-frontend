7de49235cfca1b734a2b511abcd281fce91a883d
Ji-Wei Hong <jiweihong0@gmail.com>
2022-10-31 11:09:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/lovego_frontend'>
+    <Router basename='/'>
       <App />
     </Router>
   </React.StrictMode>
